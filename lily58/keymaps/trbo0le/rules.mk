b34e2fad051f4bca9eaa8e81541f6b7585a9ed76
trbo0le <125277093+trbo0le@users.noreply.github.com>
2025-08-03 13:58:28 +0200

    Add files via upload
    keymap.json is not an original, so that is what I converted from to C when discovering limitations of using .json as keymap.
    Original keymap.json = https://github.com/qmk/qmk_firmware/blob/master/keyboards/splitkb/aurora/lily58/keymaps/default/keymap.json

---
+++ lily58/keymaps/trbo0le/rules.mk
@@ -0,0 +1,14 @@
+# Enable OLED displays
+OLED_ENABLE = yes
+
+# Enable RGB matrix (the modern RGB system)
+RGB_MATRIX_ENABLE = yes
+
+# Disable old rgblight system (as specified in JSON)
+RGBLIGHT_ENABLE = no
+
+# Enable rotary encoders
+ENCODER_ENABLE = yes
+
+# Optional: Enable split keyboard support (recommended for Lily58)
+SPLIT_KEYBOARD = yes
